24adea68c2cda05553398db94d00403fe8622970
Akshan Dhyani <akshandhyani987@gmail.com>
2026-04-28 23:49:00 +0530

    clean firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,14 @@
 import admin from "firebase-admin";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 const serviceAccount = {
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../serviceAccountKey.json"), "utf-8")
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
