2a9d8efd512e6774f7cd0a7491facaa4fb5135d8
Akshan Dhyani <akshandhyani987@gmail.com>
2026-04-28 23:58:34 +0530

    updated firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 import admin from "firebase-admin";
 
 const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
