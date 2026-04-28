acd27892192622599b32be027ece39bf0b535692
Akshan Dhyani <akshandhyani987@gmail.com>
2026-04-28 23:40:30 +0530

    updated firebase.js for authentication

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,16 @@
 import admin from "firebase-admin";
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import admin from "firebase-admin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+const serviceAccount = {
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 const serviceAccount = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../serviceAccountKey.json"), "utf-8")
@@ -13,5 +19,6 @@ const serviceAccount = JSON.parse(
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 
 export default admin;
