77bdf48be377c1e5c6770794e914a8c933bd7454
Akshan Dhyani <akshandhyani987@gmail.com>
2026-04-29 13:27:25 +0530

    fixed index.js url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://medi-log-frontend.vercel.app",
+      "https://medilog-henna.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
